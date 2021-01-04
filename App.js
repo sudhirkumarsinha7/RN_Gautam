e81b07f1cc477b8729908e5f84c8958a3f449414
AnandNamoju <gautty95p@gmail.com>
2021-01-04 18:52:40 +0530

    Registration Page

--- App.js
+++ App.js
@@ -7,10 +7,7 @@ class App extends Component{
   render(){
     return(
       <ScrollView>
-        <Regi
-        Gautam = {'Gupta'}/>
-        <Regi
-        Gautam = {'Gupta'}/>
+        <Regi/>
       </ScrollView>
         
     );
